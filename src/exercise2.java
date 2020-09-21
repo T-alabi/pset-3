533055f518888cb1db8353757852402893f371c4
T-alabi <talabi@ucvts.org>
2020-09-20 21:10:34 -0400

    Initial commit

---
+++ src/exercise2.java
@@ -0,0 +1,6 @@
+public class exercise2 {
+    public static void main(String[] args){
+        String message = " \"The answer's quite simple,\" the professor said. \"Just think outside the box!\"  ";
+        System.out.println(message);
+    }
+}
